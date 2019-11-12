e6ed0aceccf80d2d5c7ce0cdee112b0ff59fdeb2
rousseaualexy <w"sfL!YLt8Xmxw+>
2019-11-12 09:03:32 +0100

    Début d'implémentation d'algorithme de Dijkstra
    - Création de prototypes pour A*, BFS
    - Création du pattern Strategy pour Heuristic & InformedHeuristic

---
+++ src/planning/SimpleHeuristic.java
@@ -0,0 +1,12 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package planning;
+
+public class SimpleHeuristic implements Heuristic {
+   public count(State s) {
+       return 0;
+   }
+}
